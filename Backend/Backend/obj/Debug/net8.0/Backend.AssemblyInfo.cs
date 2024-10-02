6f6763c01f090d695b91fc93759316b84139e495
Martina Kay Basquez <martinabasquez@gmail.com>
2024-10-01 20:44:01 -0500

    generated model

--- Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0835c87348e7b54b7a9d5be181ffbb3268a14f16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fed567cacb194659eb5f5ae49e6b261ca84535e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Frontend/src/app/models/favorite.ts
@@ -0,0 +1,7 @@
+export interface Favorite {
+    id:number;
+    description:string;
+    restaurant:string;
+    rating:number;
+    orderAgain:boolean;
+}
